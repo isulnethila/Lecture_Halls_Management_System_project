01c41f1f02c7d57a8d423d0c79de046f93481e08
madushilakmini <139555956+madushilakmini@users.noreply.github.com>
2024-06-20 23:02:38 +0530

    Create timeSlotRoutes.js

---
+++ backend/Routes/timeSlotRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const TimeSlot = require('../models/TimeSlot');
+
+router.post('/add', async (req, res) => {
+    const timeSlot = new TimeSlot(req.body);
+    await timeSlot.save();
+    res.send(timeSlot);
+});
+
+router.get('/', async (req, res) => {
+    const timeSlots = await TimeSlot.find();
+    res.send(timeSlots);
+});
+
+module.exports = router;
